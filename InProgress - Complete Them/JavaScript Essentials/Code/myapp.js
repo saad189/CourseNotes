90f297194bf6b7abb44a4650db9026dd57cfbd10
saad1891 <saad.ahmed@daoproptech.com>
2021-04-12 02:05:08 +0500

    Completed Basic Syntax for JS Essentials

--- InProgress - Complete Them/JavaScript Essentials/Code/myapp.js
+++ InProgress - Complete Them/JavaScript Essentials/Code/myapp.js
@@ -4,16 +4,16 @@ var object = {
     prop: this,
     embed:
     {
-        method: function(){ return this; }
+        method: function () { return this; }
     }
 };
 
 var array = [
     this,
-    function(){ return this; }
+    function () { return this; }
 ];
 
-function global(){
+function global() {
     return this;
 }
 
@@ -23,11 +23,31 @@ object.embed.method();
 array[1]();
 
 // Assign context
-global.call( object );
-object.embed.method.call( object );
-array[1].call( object );
+global.call(object);
+object.embed.method.call(object);
+array[1].call(object);
 
 // New context
 new global();
-new object.embed.method( object );
+new object.embed.method(object);
 new array[1]();
+
+function Apple(x, y, color, score) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.score = score;
+}
+
+
+Apple.prototype = {
+    // eat() { return "eating the apple" },
+    eat() {return this},
+    throw(){ return "throwing the apple" }
+}
+
+var first_apple = new Apple(10, 5, 'yellow', 5);
+var second_apple = new Apple(10, 5, 'red', 5);
+var third_apple = new Apple(10, 5, 'blue', 5);
+console.log('First Apple:', first_apple);
+
