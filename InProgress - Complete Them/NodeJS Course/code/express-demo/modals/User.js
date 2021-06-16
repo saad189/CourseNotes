4fdad7c873934eb1d4939fb835fcd9f16f20ce82
saad18910@hotmail.com <saad18910@hotmail.com>
2021-06-16 12:27:43 +0500

    Sort of Completed TDD

--- InProgress - Complete Them/NodeJS Course/code/express-demo/modals/User.js
+++ InProgress - Complete Them/NodeJS Course/code/express-demo/modals/User.js
@@ -16,13 +16,19 @@ const userSchema = new mongoose.Schema({
   },
   password: { type: String, required: true, minlength: 8, maxlength: 1024 },
   isAdmin: Boolean,
+  qualification: String,
 });
 
+// Attaching a method
 userSchema.methods.generateAuthToken = function () {
   return jwt.sign(
-    _.pick(this, ["_id", "email", "name", "isAdmin"]),
+    _.pick(this, ["_id", "email", "name", "isAdmin", "qualification"]),
     config.get("jwtPrivateKey")
   );
+};
+
+userSchema.statics.lookup = function (email) {
+  return this.findOne({ email: email });
 };
 const User = mongoose.model("User", userSchema);
 
@@ -31,6 +37,7 @@ function validateUser(user) {
     name: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(50).required().email(),
     password: Joi.string().min(8).max(255).required(),
+    qualification: Joi.string(),
   });
   return schema.validate(user);
 }
---
+++ InProgress - Complete Them/NodeJS Course/code/express-demo/tests/test-driven/x.js
@@ -0,0 +1,7 @@
+// POST /api/qualification {email}
+
+// Return 401 if client is not logged in
+// Return 400 if email is not provided
+// Return 404 if no qualification is found for this user
+// Return 200 if valid request
+// get the qualification as well
