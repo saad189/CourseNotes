2436160552f0197cd43408ccd250181448b4b345
saadAhmed <saad.ahmed@empglabs.com>
2021-12-23 15:14:46 +0500

    Working on Section 3

--- InProgress - Complete Them/React/code/01-starting-setup/src/components/ExpenseItem.js
+++ InProgress - Complete Them/React/code/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
-function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+import "./ExpenseItem.css";
+import "./ExpenseDate";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItem({ title, amount, date }) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
---
+++ InProgress - Complete Them/React/code/01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,32 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses({ expenses }) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
